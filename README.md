410b222e74eaf2b3e249c23a59c28457268485d3
Micael Rodrigues <micaelr95@outlook.pt>
2022-04-18 22:16:32 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# NextJS Hardhat template
+
+This template uses [Next.js](https://nextjs.org/), [Tailwind](https://tailwindcss.com/) and [Hardhat](https://hardhat.org/) and [Typescript](https://www.typescriptlang.org/).
+
+The template comes with a sample contract, a test for the contract and a task to deploy the contract. Also comes preconfigured with NextJS, Tailwind and Typescript.
---
+++ hardhat.config.ts
@@ -0,0 +1,63 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+task("deploy", "Deploy smart contracts", async (taskArgs, hre) => {
+  const Greeter = await hre.ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, Hardhat!");
+
+  await greeter.deployed();
+
+  console.log("Greeter deployed to:", greeter.address);
+
+  await hre.run("verify:verify", {
+    address: greeter.address,
+    constructorArguments: ["Greeter", "Gtr"],
+  });
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.4",
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  paths: {
+    sources: "./ethereum/contracts",
+    tests: "./ethereum/test",
+    cache: "./ethereum/cache",
+    artifacts: "./ethereum/artifacts",
+  },
+};
+
+export default config;
